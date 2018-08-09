9168a2c0c030d15834a29ce8e4b1e28999dc572f
我的名字 <3269983308@qq.com>
2018-08-09 20:35:42 +0800

    0809

---
+++ dao/src/com/nf147/ldl/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.nf147.ldl.dao;
+
+import com.nf147.ldl.entity.Student;
+
+public class StudentDAO {
+    public int insertStudent(Student student){
+        return 1;
+    }
+}
---
+++ entity/src/com/nf147/ldl/entity/Student.java
@@ -0,0 +1,42 @@
+package com.nf147.ldl.entity;
+
+public class Student {
+    private String name;
+    private String sex;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(){
+
+    }
+
+    public Student(String name,String sex,int age){
+        this.name=name;
+        this.sex=sex;
+        this.age=age;
+    }
+
+}
